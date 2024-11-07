a6b429359485475f5bed00c9d075bc48d7f70e42
hola-soy-milk <hola@carmenh.dev>
2024-11-07 18:05:03 +0100

    Initial commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<script setup>
+const result = await fetch('/api/img');
+console.log(result);
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
---
+++ server/api/img.ts
@@ -0,0 +1,17 @@
+import { createDirectus, rest, readAssetRaw } from '@directus/sdk';
+export default defineEventHandler(async (event) => {
+  const FILE_ID = '9be59ee6-4617-488e-aac8-eb47e046e007';
+
+  const directus = createDirectus(
+    'http://photography.directus.local:8055'
+  ).with(rest());
+  const result = await directus.request(
+    readAssetRaw(FILE_ID, {
+      width: 300,
+      height: 300,
+      quality: 50,
+      fit: 'contain',
+    })
+  );
+  return { result };
+});
